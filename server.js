7dd03b33b4b743d9e7ae06a75678506cd36a10e5
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-12 16:19:29 -0700

    set up basic server routes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require('path');
+
+const exportHtmlRoutes = require('./app/routing/htmlRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'app/public/home.html'));
-});
+// add /survey
+exportHtmlRoutes(app);
 
 app.listen(PORT, () => {
 console.log(`App is listening on port ${PORT}`);
