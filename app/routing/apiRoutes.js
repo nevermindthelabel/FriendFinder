604c0c4ec37618d0e13c68c7b62b54a967e1bf28
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-12 16:22:57 -0700

    basic workflow for displaying api json data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+
 /*
 Your `apiRoutes.js` file should contain two routes:
 
@@ -11,6 +12,7 @@ const friends = require('../data/friends.js');
 const app = express();
 
 app.get('/api/friends', (request, response) => response.json(friends));
+console.log(friends)
 
 app.post('/api/friends', (request, response) => {
   const newFriends = request.body;
