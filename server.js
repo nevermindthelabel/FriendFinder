20f4ae20af7bbc4663040f162426d24bf7e96064
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-15 07:39:14 -0700

    created basic server

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const path = require('path');
+
+// const logicCss = path.join(__dirname, 'app/public');
+// const friendsArray = path.join(__dirname, 'app/data');
+// const bodyParser = require('body-parser');
 
 const exportHtmlRoutes = require('./app/routing/htmlRoutes')
+const friendsApi = require ('./app/routing/apiRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // middleware
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('app/public'));
+app.use('/app/data/friends.js', express.static(`${__dirname  }/app/data/friends.js`));
 
-// add /survey
 exportHtmlRoutes(app);
+friendsApi(app);
 
 app.listen(PORT, () => {
 console.log(`App is listening on port ${PORT}`);
