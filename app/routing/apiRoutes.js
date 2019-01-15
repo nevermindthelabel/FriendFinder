f859679c0717d23641c29cb5a0d6a0aced4682d3
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-15 07:41:33 -0700

    added get and post requests

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,17 @@
-
-/*
-Your `apiRoutes.js` file should contain two routes:
-
-   * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-   * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-*/
-const express = require('express');
-// const path = require('path');
 const friends = require('../data/friends.js');
 
-const app = express();
+function friendsApi(app) {
 
-app.get('/api/friends', (request, response) => response.json(friends));
-console.log(friends)
+  app.get('/api/friends', (request, response) => {
+    response.json(friends)
+    // console.log(friends)
+  });
 
-app.post('/api/friends', (request, response) => {
-  const newFriends = request.body;
-  friends.push(newFriends);
-});
+  app.post('/api/friends', (request, response) => {
+    const newFriends = request.body;
+    console.log(newFriends);
+    friends.push(newFriends);
+    response.json(newFriends);
+  })
+}
+module.exports = friendsApi;
