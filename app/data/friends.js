0059ea0954d6c7649d34599e0042ad6d61a8b7b0
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-19 15:11:18 -0700

    modifications and changes to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 const friends = [
   {
     name: 'Matt',
-    photo: 'https://awkwardfamilyphotos.com/totally-tubular/',
+    photo: 'https://images.unsplash.com/photo-1518773553398-650c184e0bb3?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     scores: [
       5,
       3,
