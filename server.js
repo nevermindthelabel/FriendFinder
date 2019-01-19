f49c33434befa8734ce4c3189788256cfc74efd5
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-18 21:47:05 -0700

    fixes

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-// const logicCss = path.join(__dirname, 'app/public');
-// const friendsArray = path.join(__dirname, 'app/data');
-// const bodyParser = require('body-parser');
-
 const exportHtmlRoutes = require('./app/routing/htmlRoutes')
 const friendsApi = require ('./app/routing/apiRoutes')
 
@@ -12,12 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // middleware
-// app.use(bodyParser.urlencoded({ extended: true}));
-// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('app/public'));
-app.use('/app/data/friends.js', express.static(`${__dirname  }/app/data/friends.js`));
 
 exportHtmlRoutes(app);
 friendsApi(app);
