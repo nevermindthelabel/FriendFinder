536e10b90df7fef0485f7b242edb02edcf9a0835
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-18 21:50:16 -0700

    fixes for testing purposes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,15 +3,50 @@ const friends = require('../data/friends.js');
 function friendsApi(app) {
 
   app.get('/api/friends', (request, response) => {
-    response.json(friends)
-    // console.log(friends)
+    response.json(friends);
+   
   });
+  function compareFriendScores(scores1, scores2) {
+    let sum = 0;
+    scores1.forEach((score, index) => {
+      const difference = score - scores2[index]
+      sum += Math.abs(difference)
+
+    })
+    return sum;
+  }
+
 
   app.post('/api/friends', (request, response) => {
-    const newFriends = request.body;
-    console.log(newFriends);
-    friends.push(newFriends);
-    response.json(newFriends);
+    const newFriend = request.body;
+    let currentLowestScore = compareFriendScores(friends[0].scores, newFriend.scores);
+    let bestFriend = friends[0];
+
+    newFriend.scores = newFriend.scores.map((score) => parseInt(score, 10))
+    for (let i = 1; i < friends.length; i++) {
+      const friendScore = compareFriendScores(friends[i].scores, newFriend.scores)
+
+      if (friendScore < currentLowestScore) {
+        currentLowestScore = friendScore
+        bestFriend = friends[i];
+      }
+
+    }
+    friends.push(newFriend);
+    console.log(friends);
+    console.log(bestFriend);
+    console.log(`
+    ${bestFriend.name},
+    ${bestFriend.photo},
+    ${bestFriend.scores}`);
+
+    response.json(bestFriend);
+
   })
 }
 module.exports = friendsApi;
+
+// function modalLogic() {
+//   $('#myModal').modal('src', bestFriend.photo)
+// }
+// module.exports = modalLogic;
